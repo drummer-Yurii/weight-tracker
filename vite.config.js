d005c4903dd1bfdce2f4f189a47bf051127ae29c
Yurii Rybachok <drummer2009@ukr.net>
2023-11-14 09:33:12 +0200

    setup project

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import dns from 'dns';
+import vue from '@vitejs/plugin-vue';
+
+dns.setDefaultResultOrder('verbatim');
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+});
